bb41146692c15bcd571675a90ab793a7aaed6408
LeandroAlcantara-1997 <leandro1997silva97@gmail.com>
2023-03-23 00:18:32 +0000

    HSN-0001 fixed func main

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
-	"github.com/LeandroAlcantara-1997/heroes-social-network/domain/heroes"
 	"github.com/LeandroAlcantara-1997/heroes-social-network/infrastructure/config"
+	"github.com/LeandroAlcantara-1997/heroes-social-network/infrastructure/dependency"
 	v1 "github.com/LeandroAlcantara-1997/heroes-social-network/web/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv()
+	ctx := context.Background()
 	r := gin.Default()
 
-	heroService := heroes.New()
-	v1.ConfigureRoutes(r, heroService)
+	dep, err := dependency.LoadDependency(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	v1.ConfigureRoutes(r, dep.HeroUseCase)
 	log.Default().Printf("Server listening in :%s", config.Env.ApiPort)
 	r.Run(fmt.Sprintf(":%s", config.Env.ApiPort))
 
